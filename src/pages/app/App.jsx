a989a3c2cef06b09f938ba435fa2cc2e718250e8
Claudio S <claudiofresta@gmail.com>
2024-12-05 09:01:25 +0700

    main main sidebar

--- src/pages/app/App.jsx
+++ src/pages/app/App.jsx
@@ -2,6 +2,7 @@ import { Button } from "antd";
 import { ProForm, ProFormText } from "@ant-design/pro-components";
 import { Layout } from "antd";
 import { ProLayout, PageContainer } from "@ant-design/pro-components";
+import { HomeOutlined, UserOutlined, GithubOutlined } from "@ant-design/icons";
 import "./App.css";
 
 const { Header, Sider, Content } = Layout;
@@ -14,6 +15,27 @@ function App() {
         logo="https://avatars.githubusercontent.com/u/19315843?s=200&v=4" // Replace with your logo URL
         layout="side" // Sidebar layout
         location={{ pathname: window.location.pathname }} // Make sure to sync with the current path
+        menuDataRender={() => [
+          {
+            path: "/",
+            name: "Home",
+            icon: <HomeOutlined />,
+          },
+          {
+            path: "/about",
+            name: "About",
+            icon: <UserOutlined />,
+          },
+          {
+            path: "/contact",
+            name: "Contact",
+            icon: <GithubOutlined />,
+          },
+        ]}
+        // Menu item styling
+        menuItemRender={(menuItemProps, defaultDom) => (
+          <div style={{ color: "white" }}>{defaultDom}</div> // White color for menu item text
+        )}
       >
         <PageContainer
           content="Welcome to the ProLayout component"
@@ -34,9 +56,7 @@ function App() {
               Primary Action
             </Button>,
           ]}
-        >
-          {/* {children} */}
-        </PageContainer>
+        ></PageContainer>
       </ProLayout>
     </>
   );
